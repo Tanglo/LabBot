b1b838cf9008c1ca93ed565e580483e8d30b4033
Lee Walsh <l.walsh@Lees-iMac.local>
2014-04-09 17:12:04 +1000

    Initial Commit

---
+++ LabBot/LabBot.h
@@ -0,0 +1,13 @@
+//
+//  LabBot.h
+//  LabBot
+//
+//  Created by Lee Walsh on 9/04/2014.
+//  Copyright (c) 2014 Dead Rubber Hand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LabBot : NSObject
+
+@end
---
+++ LabBot/LabBot.m
@@ -0,0 +1,13 @@
+//
+//  LabBot.m
+//  LabBot
+//
+//  Created by Lee Walsh on 9/04/2014.
+//  Copyright (c) 2014 Dead Rubber Hand. All rights reserved.
+//
+
+#import "LabBot.h"
+
+@implementation LabBot
+
+@end
