576bc94050aa1dc93eb6d93a48fb0ebe37b55f11
Tanglo <developer@deadrubberhand.com>
2014-08-12 16:12:59 +1000

    Changes to DRHDataMatrix
    Deprecated dataMatrixWithColumnArrays:
    dataMatrixWithDataColumnArray: replaces dataMatrixWithColumnArrays:
    Several new methods.
    Fixed bug in initWithCoder: (i.e. loading saved data)

--- DRHDataMatrix.h
+++ DRHDataMatrix.h
@@ -19,14 +19,20 @@ extern NSString * const DRHDataMatrixFactorNamesKey;
 @property NSArray *data;
 
 -(DRHDataMatrix *)initWithRowArrays:(NSArray *)rowArrays;
-+(DRHDataMatrix *)dataMatrixWithColumnArrays:(NSArray *)dataArrays;
++(DRHDataMatrix *)dataMatrixWithColumnArrays:(NSArray *)dataArrays __attribute__((deprecated));
++(DRHDataMatrix *)dataMatrixWithDataColumnArray:(NSArray *)dataArrays;
 -(NSInteger)rowCount;
 -(NSInteger)columnCount;
 -(NSArray *)dataRowAtIndex:(NSInteger)index;
+-(NSArray *)dataRowAtIndex:(NSInteger)index ForColumnsWithIndexes:(NSIndexSet *)columnIndexes;
+-(NSArray *)dataRowAtIndex:(NSInteger)index ForColumnsWithNames:(NSArray *)nameArray;
+-(NSInteger)indexOfDataColumnWithName:(NSString *)columnName;
+-(NSIndexSet *)indexesOfDataColumnsWithNames:(NSArray *)nameArray;
 -(DRHDataColumn *)dataColumnAtIndex:(NSInteger)index;
 -(DRHDataColumn *)dataColumnWithName:(NSString *)name;
 -(DRHDataMatrix *)dataMatrixByAddingDataColumn:(DRHDataColumn *)dataColumn;
 -(DRHDataMatrix *)dataMatrixByInsertingColumn:(DRHDataColumn *)dataColumn AtIndex:(NSInteger)index;
+-(DRHDataMatrix *)dataMatrixByRemovingColumnWithName:(NSString *)columnName;
 -(void)shuffleRows;
 
 @end
--- DRHDataMatrix.m
+++ DRHDataMatrix.m
@@ -28,7 +28,7 @@ NSString * const DRHDataMatrixFactorNamesKey = @"trialMatrixFactorNames";
         if (!_names) {
             _names = nil;
         }
-        _data = [aDecoder decodeObjectForKey:@"dataArray"];
+        _data = [aDecoder decodeObjectForKey:@"data"];
         if (!_data) {
             _data = nil;
         }
@@ -70,6 +70,26 @@ NSString * const DRHDataMatrixFactorNamesKey = @"trialMatrixFactorNames";
     return newDataMatrix;
 }
 
++(DRHDataMatrix *)dataMatrixWithDataColumnArray:(NSArray *)dataArrays{
+    NSInteger numberOfColumns = [dataArrays count];
+    NSInteger numberOfRows = [[dataArrays objectAtIndex:0] count];
+    NSMutableArray *newTrialRows = [NSMutableArray array];
+    for (NSInteger i=0; i<numberOfRows; i++) {
+        NSMutableArray *newRowArray = [NSMutableArray array];
+        for (NSInteger j=0; j<numberOfColumns; j++) {
+            [newRowArray addObject:[[dataArrays objectAtIndex:j] rowAtIndex:i]];
+        }
+        [newTrialRows addObject:newRowArray];
+    }
+    DRHDataMatrix *newDataMatrix = [[DRHDataMatrix alloc] initWithRowArrays:newTrialRows];
+    NSMutableArray *newNameArray = [NSMutableArray array];
+    for (NSInteger i=0; i<numberOfColumns; i++) {
+        [newNameArray addObject:[[dataArrays objectAtIndex:i] columnName]];
+    }
+    [newDataMatrix setNames:[NSArray arrayWithArray:newNameArray]];
+    return newDataMatrix;
+}
+
 -(NSInteger)rowCount{
     return [_data count];
 }
@@ -80,6 +100,40 @@ NSString * const DRHDataMatrixFactorNamesKey = @"trialMatrixFactorNames";
 
 -(NSArray *)dataRowAtIndex:(NSInteger)index{
     return [_data objectAtIndex:index];
+}
+
+-(NSArray *)dataRowAtIndex:(NSInteger)index ForColumnsWithIndexes:(NSIndexSet *)columnIndexes{
+    NSArray *dataRowArray = [self dataRowAtIndex:index];
+    NSMutableArray *newDataArray = [NSMutableArray array];
+    for (NSInteger i=0; i<dataRowArray.count; i++) {
+        if ([columnIndexes containsIndex:i])
+            [newDataArray addObject:[dataRowArray objectAtIndex:i]];
+    }
+    return [NSArray arrayWithArray:newDataArray];
+}
+
+-(NSArray *)dataRowAtIndex:(NSInteger)index ForColumnsWithNames:(NSArray *)nameArray{
+    NSIndexSet *indexSet = [self indexesOfDataColumnsWithNames:nameArray];
+    return [self dataRowAtIndex:index ForColumnsWithIndexes:indexSet];
+}
+
+-(NSInteger)indexOfDataColumnWithName:(NSString *)columnName{
+    for (NSInteger i=0; i<[_names count]; i++) {
+        if ([[_names objectAtIndex:i] isEqualToString:columnName])
+            return i;
+    }
+    return NSNotFound;
+}
+
+-(NSIndexSet *)indexesOfDataColumnsWithNames:(NSArray *)nameArray{
+    NSMutableIndexSet *indexSet = [NSMutableIndexSet indexSet];
+    for (NSString *currentName in nameArray){
+        for (NSInteger i=0; i<[_names count]; i++){
+            if ([currentName isEqualToString:[_names objectAtIndex:i]])
+                [indexSet addIndex:i];
+        }
+    }
+    return indexSet;
 }
 
 -(DRHDataColumn *)dataColumnAtIndex:(NSInteger)index{
@@ -104,7 +158,7 @@ NSString * const DRHDataMatrixFactorNamesKey = @"trialMatrixFactorNames";
             [dataColumnArray addObject:[self dataColumnAtIndex:i]];
         }
         [dataColumnArray addObject:dataColumn];
-        return [DRHDataMatrix dataMatrixWithColumnArrays:dataColumnArray];
+        return [DRHDataMatrix dataMatrixWithDataColumnArray:dataColumnArray];
     }
     return nil;
 }
@@ -119,7 +173,22 @@ NSString * const DRHDataMatrixFactorNamesKey = @"trialMatrixFactorNames";
         for (NSInteger i=index; i<[self columnCount]; i++) {
             [dataColumnArray addObject:[self dataColumnAtIndex:i]];
         }
-        return [DRHDataMatrix dataMatrixWithColumnArrays:dataColumnArray];
+        return [DRHDataMatrix dataMatrixWithDataColumnArray:dataColumnArray];
+    }
+    return nil;
+}
+
+-(DRHDataMatrix *)dataMatrixByRemovingColumnWithName:(NSString *)columnName{
+    NSInteger columnIndex = [self indexOfDataColumnWithName:columnName];
+    if (columnIndex == NSNotFound) {
+        return self;
+    } else {
+        NSMutableArray *dataColumnsToKeep = [NSMutableArray array];
+        for (NSInteger i=0; i<[self columnCount]; i++){
+            if (i != columnIndex)
+                [dataColumnsToKeep addObject:[self dataColumnAtIndex:i]];
+        }
+        return [DRHDataMatrix dataMatrixWithDataColumnArray:dataColumnsToKeep];
     }
     return nil;
 }
