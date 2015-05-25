b7d0308ce9eb6878c4de8d21ca540c5b5f6c9388
Tanglo <l.walsh@neura.edu.au>
2015-05-25 13:45:34 +1000

    Added LBImageView

---
+++ LBImageView.swift
@@ -0,0 +1,20 @@
+//
+//  LBImageView.swift
+//  LabBot
+//
+//  Created by Lee Walsh on 25/05/2015.
+//  Copyright (c) 2015 Lee David Walsh. All rights reserved.
+//
+
+import Cocoa
+
+public class LBImageView: NSView {
+    var image: NSImage?
+
+    public override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
